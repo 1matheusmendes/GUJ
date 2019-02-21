c5d912c1c9d3ddbecee265800a08bc4b18e2b566
1matheusmendes <matheus_mendess@live.com>
2019-02-21 14:22:35 -0300

    Primeiro Exercicio

--- src/conceitosBasicos/areaTriangulo.java
+++ src/conceitosBasicos/areaTriangulo.java
@@ -1,7 +1,22 @@
 package conceitosBasicos;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class areaTriangulo {
     public static void main(String[] args) {
+        double area;
 
+        String base = JOptionPane.showInputDialog("Digite a base ");
+        String altura = JOptionPane.showInputDialog("Digite a altura ");
+
+        Scanner ler = new Scanner(System.in);
+
+        double baseConvertida = Double.parseDouble(base);
+        double alturaConvertida = Double.parseDouble(altura);
+
+        area = alturaConvertida*baseConvertida / 2;
+
+        System.out.println("A area do triangulo é: "+area);
     }
 }
---
+++ src/conceitosBasicos/bhaskara.java
@@ -0,0 +1,16 @@
+package conceitosBasicos;
+
+public class bhaskara {
+    public static void main(String[] args) {
+        final int a = 1;
+        final int b = 12;
+        final int c = -13;
+
+        double resultado;
+
+        resultado = b*b - 4*a*c;
+
+        System.out.println("Delta é: "+resultado);
+
+    }
+}
